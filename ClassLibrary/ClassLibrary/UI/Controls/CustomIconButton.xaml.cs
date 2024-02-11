f13a81195b0924b181767b0c4e8a78d74ba2f52a
Austin <austintwalters@gmail.com>
2024-02-10 22:55:35 -0500

    Added CustomIconButton, and changed CustomButton to CustomTextButton. Need to deal with properties being defined by multiple user controls. WIP

---
+++ ClassLibrary/ClassLibrary/UI/Controls/CustomIconButton.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ClassLibrary.UI.Controls
+{
+    public partial class CustomIconButton : UserControl
+    {
+        private static readonly SolidColorBrush defaultBackgroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+        private static readonly SolidColorBrush defaultForegroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+        private static readonly SolidColorBrush defaultMouseOverBackgroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+        private static readonly SolidColorBrush defaultMouseOverForegroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
+        private static readonly int defaultCornerRadius = 5;
+
+        public static readonly DependencyProperty ButtonCommandProperty = DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(CustomTextButton));
+        public ICommand ButtonCommand
+        {
+            get { return (ICommand)GetValue(ButtonCommandProperty); }
+            set { SetValue(ButtonCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonForegroundProperty = DependencyProperty.Register("ButtonForeground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultForegroundBrush));
+        public Brush ButtonForeground
+        {
+            get { return (Brush)GetValue(ButtonForegroundProperty); }
+            set { SetValue(ButtonForegroundProperty, value); }
+        }
+        public static readonly DependencyProperty ButtonBackgroundProperty = DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultBackgroundBrush));
+        public Brush ButtonBackground
+        {
+            get { return (Brush)GetValue(ButtonBackgroundProperty); }
+            set { SetValue(ButtonBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty MouseOverBackgroundProperty = DependencyProperty.Register("MouseOverBackground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultMouseOverBackgroundBrush));
+        public Brush MouseOverBackground
+        {
+            get { return (Brush)GetValue(MouseOverBackgroundProperty); }
+            set { SetValue(MouseOverBackgroundProperty, value); }
+        }
+        public static readonly DependencyProperty MouseOverForegroundProperty = DependencyProperty.Register("MouseOverForeground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultMouseOverForegroundBrush));
+        public Brush MouseOverForeground
+        {
+            get { return (Brush)GetValue(MouseOverForegroundProperty); }
+            set { SetValue(MouseOverForegroundProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(int), typeof(CustomTextButton), new PropertyMetadata(defaultCornerRadius));
+        public int CornerRadius
+        {
+            get { return (int)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public CustomIconButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ClassLibrary/ClassLibrary/UI/Controls/CustomTextButton.xaml.cs
+++ ClassLibrary/ClassLibrary/UI/Controls/CustomTextButton.xaml.cs
@@ -3,58 +3,58 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace ClassLibrary
+namespace ClassLibrary.UI.Controls
 {
-    public partial class CustomButton : UserControl
+    public partial class CustomTextButton : UserControl
     {
         private static readonly SolidColorBrush defaultBackgroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
         private static readonly SolidColorBrush defaultForegroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
         private static readonly SolidColorBrush defaultMouseOverBackgroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
         private static readonly SolidColorBrush defaultMouseOverForegroundBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#000000"));
         private static readonly int defaultCornerRadius = 15;
-        public static readonly DependencyProperty ButtonContentProperty = DependencyProperty.Register("ButtonContent", typeof(string), typeof(CustomButton));
+        public static readonly DependencyProperty ButtonContentProperty = DependencyProperty.Register("ButtonContent", typeof(string), typeof(CustomTextButton));
         public string ButtonContent
         {
             get { return (string)GetValue(ButtonContentProperty); }
             set { SetValue(ButtonContentProperty, value); }
         }
-        public static readonly DependencyProperty ButtonCommandProperty = DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(CustomButton));
+        public static readonly DependencyProperty ButtonCommandProperty = DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(CustomTextButton));
         public ICommand ButtonCommand
         {
             get { return (ICommand)GetValue(ButtonCommandProperty); }
             set { SetValue(ButtonCommandProperty, value); }
         }
-        public static readonly DependencyProperty ButtonForegroundProperty = DependencyProperty.Register("ButtonForeground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(defaultForegroundBrush));
+        public static readonly DependencyProperty ButtonForegroundProperty = DependencyProperty.Register("ButtonForeground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultForegroundBrush));
         public Brush ButtonForeground
         {
             get { return (Brush)GetValue(ButtonForegroundProperty); }
             set { SetValue(ButtonForegroundProperty, value); }
         }
-        public static readonly DependencyProperty ButtonBackgroundProperty = DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(defaultBackgroundBrush));
+        public static readonly DependencyProperty ButtonBackgroundProperty = DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultBackgroundBrush));
         public Brush ButtonBackground
         {
             get { return (Brush)GetValue(ButtonBackgroundProperty); }
             set { SetValue(ButtonBackgroundProperty, value); }
         }
-        public static readonly DependencyProperty MouseOverBackgroundProperty = DependencyProperty.Register("MouseOverBackground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(defaultMouseOverBackgroundBrush));
+        public static readonly DependencyProperty MouseOverBackgroundProperty = DependencyProperty.Register("MouseOverBackground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultMouseOverBackgroundBrush));
         public Brush MouseOverBackground
         {
             get { return (Brush)GetValue(MouseOverBackgroundProperty); }
             set { SetValue(MouseOverBackgroundProperty, value); }
         }
-        public static readonly DependencyProperty MouseOverForegroundProperty = DependencyProperty.Register("MouseOverForeground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(defaultMouseOverForegroundBrush));
+        public static readonly DependencyProperty MouseOverForegroundProperty = DependencyProperty.Register("MouseOverForeground", typeof(Brush), typeof(CustomTextButton), new PropertyMetadata(defaultMouseOverForegroundBrush));
         public Brush MouseOverForeground
         {
             get { return (Brush)GetValue(MouseOverForegroundProperty); }
             set { SetValue(MouseOverForegroundProperty, value); }
         }
-        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(int), typeof(CustomButton), new PropertyMetadata(defaultCornerRadius));
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(int), typeof(CustomTextButton), new PropertyMetadata(defaultCornerRadius));
         public int CornerRadius
         {
             get { return (int)GetValue(CornerRadiusProperty); }
             set { SetValue(CornerRadiusProperty, value); }
         }
-        public CustomButton()
+        public CustomTextButton()
         {
             InitializeComponent();
         }
